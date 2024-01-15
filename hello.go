3f30654694bedeb7a86d52df20bf137445875d61
shivam singh <93473684+Basically-a-programmer@users.noreply.github.com>
2024-01-15 22:02:25 +0530

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// types of variable
+	//  bool , string
+	// int , uint (unsigned int) , float , complex , byte, rune ( char)
+	//  you can have auto interpreter for variable declaration using
+
+	total := 0 // auto declare
+
+	average, message := 23.6, "it's the average" // multiple declaration
+	var num int = int(average)
+	const pul int = 40 // constant
+	total = num + pul
+	fmt.Println("total", total)
+	fmt.Println("the average", average)
+	fmt.Println("the display", message)
+	fmt.Println("sum = ", num+pul)
+
+}
